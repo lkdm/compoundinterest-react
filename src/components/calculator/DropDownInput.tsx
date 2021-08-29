af2929f39ac543f57183752b0cca3d707fd4c5ca
luknuk <44519853+luknuk@users.noreply.github.com>
2021-08-29 10:09:55 +1000

    Change drop down form options to interval.

--- src/components/calculator/DropDownInput.tsx
+++ src/components/calculator/DropDownInput.tsx
@@ -12,9 +12,11 @@ const DropDownInput: React.FC<Props> = ({label}) => {
       </label>
       <select className="form-select" aria-label="{label}">
         <option selected>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <option value="1">Annually</option>
+        <option value="2" selected>Monthly</option>
+        <option value="3">Fortnightly</option>
+        <option value="3">Weekly</option>
+        <option value="3">Daily</option>
       </select>
     </div>
   )
