b41c647b8c9a9fb520fd1b6dea6390c5a3878dfb
luknuk <44519853+luknuk@users.noreply.github.com>
2021-08-30 15:35:21 +1000

    Basic compound interest calculation.

--- src/services/CompoundInterest.tsx
+++ src/services/CompoundInterest.tsx
@@ -13,9 +13,36 @@ Calculate Compound Interest
   annualInterestRate: number
 */
 
-export const calculateCompoundInterest = (strategy: Strategy) => {
-  const P = strategy.initialDeposit
-  const i = 1 + strategy.annualInterestRate/100
+// Find new principal each compound:
+// newPrincipal = currentPrincipal * (1 + r)
+// Find interest rate per compound (where f is compound)
+// r/f
 
-  return P*i
+// Calculate interest rate for each compound, given annual rate and compounds/year
+const ratePerCompound = (ratePerAnnum: number, compoundsPerYear: number): number => ratePerAnnum/compoundsPerYear
+const simpleInterest = (r: number, p: number): number => p*r
+
+
+export const calculateCompoundInterest = (strategy: Strategy) => {
+  // R = Rate per annum
+  const R: number = strategy.annualInterestRate/10000
+
+  let r: number // Rate per compound
+  let F: number // Compound frequency
+  if (strategy.compoundFrequency === "Monthly") {
+    r = ratePerCompound(R, 12)
+    F = 12*strategy.numberOfYears // 12 compounds per year
+  } else { // Per annum
+    r = R
+    F = strategy.numberOfYears
+  }
+
+  // Principal
+  let P = strategy.initialDeposit
+
+  for (let n:number = 0; n< F; n++) {
+    P = P * (1 + r)
+  }
+
+  return P
 }
