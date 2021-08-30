76ba9657b1a388d67d3463e2e79f2f4719f408f3
luknuk <44519853+luknuk@users.noreply.github.com>
2021-08-30 14:35:34 +1000

    Create abstract types for Money and Percent

--- src/store/types.tsx
+++ src/store/types.tsx
@@ -1,8 +1,11 @@
 export type Frequency = "Annually" | "Monthly" | "Fortnightly" | "Weekly" | "Daily"
 
+export type Money = number
+export type Percent = number
+
 export interface Strategy {
-  initialDeposit: number,
-  regularDeposit: number,
+  initialDeposit: Money,
+  regularDeposit: Money,
   depositFrequency: Frequency,
   compoundFrequency: Frequency,
   numberOfYears: number,
