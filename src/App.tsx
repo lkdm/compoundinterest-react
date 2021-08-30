ccc238b1a799223600258dd4e248c2dd9456fa88
luknuk <44519853+luknuk@users.noreply.github.com>
2021-08-30 13:39:47 +1000

    Clean up input cleaning functions.

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const defaultStrategy: Strategy = {
   annualInterestRate: 5
 }
 
-const cleanNumber = (rawInput: string): number => {
+const cleanNumber = (rawInput: string, min?: number, max?: number): number => {
   /*
     Accepts a string and casts it into a number
     - Disallows alpha & special characters
@@ -28,49 +28,18 @@ const cleanNumber = (rawInput: string): number => {
   const cleanedInput: string = rawInput.replace(/[^0-9]/g, "")
 
   // Cast result to number
-  const result: number = Number(cleanedInput)
+  let result: number = Number(cleanedInput)
+
+  if (min && result < min)
+    result = min // Min
+  if (max && result > max)
+    result = max // Max
 
   return result
 }
 const cleanMoney = cleanNumber
-const cleanPercent = (rawInput: string): number => {
-  /*
-    Accepts a string and returns a valid percent number
-    - Cast the raw input as a number
-    - Disallow percentage > 100%
-    - Disallow percentage < 0%
-    - Return percentage *100
-  */
-
-  const cleaned: number = cleanNumber(rawInput) // Clean
-  let result: number = cleaned
-  
-  if (cleaned > 10000)
-    result = 10000 // Max: 100%
-  if (cleaned < 0)
-    result = 0 // Min: 0%
-    
-  return result
-}
-const cleanYears = (rawInput: string): number => {
-  /*
-    Accepts a string and returns a valid percent number
-    - Cast the raw input as a number
-    - Disallow years > 30
-    - Disallow years < 0
-    - Return years
-  */
-  
-  const cleaned: number = cleanNumber(rawInput) // Clean
-  let result: number = cleaned
-
-  if (cleaned > 30)
-    result = 30 // Max 30
-  if (cleaned < 0)
-    result = 0
-  
-  return result
-}
+const cleanPercent = (rawInput: string): number => cleanNumber(rawInput, 0, 10000)
+const cleanYears = (rawInput: string): number => cleanNumber(rawInput, 0, 30)
 
 const App = () => {
   const [strategy, setStrategy] = useState<Strategy>(defaultStrategy)
